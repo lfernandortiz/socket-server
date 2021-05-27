5c072442932cd3d5bd7fb578f1828cac75af8afd
lfortizv <fortiz@acsendo.com>
2021-05-26 19:56:00 -0500

    Configuracion de Cors

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import Server from "./classes/server";
 import { router } from "./routes/router";
 import express from "express";
-import bodyParser from "body-parser";
+import cors from "cors";
 
 const server = new Server();
 
+//body parser
 server.app.use(express.urlencoded({ extended: true }));
 server.app.use(express.json());
 
+//cors configuracion
+server.app.use(cors({ origin: true, credentials: true }));
+
+//rutas - router
 server.app.use("/", router);
 
 server.startServer(() => {
